f75d63dec0bfcbafdf8b743a91fb242982225a2e
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2019-08-22 17:40:15 -0300

    Inicio

---
+++ RegistroClientes/RegistroClientes/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroClientes
+{
+    public class Cliente
+    {
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DataNascimento{ get; set; }
+
+
+    }
+}
