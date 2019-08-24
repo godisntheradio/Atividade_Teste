f00a25ca39a73e475470bc7cdefbb54745262f62
godisntheradio <gabriel_souzadasilva@yahoo.com.br>
2019-08-23 23:49:32 -0300

    Adição da tabela

---
+++ RegistroClientes/RegistroClientes/dbo.Clientes.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Clientes] (
+    [Id]    INT          IDENTITY (1, 1) NOT NULL,
+    [Name]  VARCHAR (50) NOT NULL,
+    [Email] VARCHAR (50) NOT NULL,
+    [DOB]   DATE         NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
